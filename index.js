6ea261cb8ba30c4d2a5e93fcb7b07c372709be81
hemant069 <hemantprajapati20500@gmail.com>
2025-01-10 21:04:21 +0530

    how i can write file using fs modules

--- index.js
+++ index.js
@@ -3,10 +3,26 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   if (req.method == "GET") {
-    fs.writeFile("log.txt", `${Date.now()}, ${req.method}`, (data, err) => {
-      return;
-    });
+    if (req.url == "/") {
+      fs.appendFile(
+        "log.txt",
+        ` ${Date.now()}, ${req.method},${req.url} \n`,
+        (data, err) => {
+          return;
+        }
+      );
+    }
+    if (req.url == "/about") {
+      fs.appendFile(
+        "log.txt",
+        ` ${Date.now()}, ${req.method} , ${req.url} \n`,
+        (data, err) => {
+          return;
+        }
+      );
+    }
   }
+
   res.end("End");
 });
 
